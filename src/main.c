e3be85d0409a4e71771719cbd6abb8dd008183ff
Yasiell Vallejos <yasi1205@hotmail.com>
2016-08-12 18:24:49 -0600

    estructura y algunas ventanas

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <gtk/gtk.h>
+#include "pending.c"
+
+int main(int argc, char *argv[]) {
+    GtkBuilder      *builder; 
+    GtkWidget       *windowMain;
+ 
+    gtk_init(&argc, &argv);
+ 
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
+ 
+    windowMain = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+ 
+    g_object_unref(builder);
+ 
+    gtk_widget_show(windowMain);                
+    gtk_main();
+ 
+    return 0;
+}
+ 
+// called when window is closed
+void on_window_main_destroy() {
+    gtk_main_quit();
+}
+
+void on_btn_exit_clicked() {
+    gtk_main_quit();
+}
+
+void on_display_pending() {
+    displayPending(); 
+}
---
+++ src/pending.c
@@ -0,0 +1,24 @@
+int displayPending() {
+    GtkBuilder      *builder; 
+    GtkWidget       *secondWindow;
+ 
+    gtk_init(NULL, NULL);
+ 
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "glade/window_pending.glade", NULL);
+ 
+    secondWindow = GTK_WIDGET(gtk_builder_get_object(builder, "window_pending"));
+    gtk_builder_connect_signals(builder, NULL);
+ 
+    g_object_unref(builder);
+ 
+    gtk_widget_show(secondWindow);                
+    gtk_main();
+ 
+    return 0;
+}
+ 
+// called when window is closed
+void on_window_pending_destroy() {
+    gtk_main_quit();
+}
