ebdd2b6d3617e74ebd653fa22bd03666de844c64
Yasiell Vallejos <yasi1205@hotmail.com>
2016-08-16 16:52:39 -0600

    Ventanas con estilo

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <string.h>
 
+int pid;
 
 int main(int argc, char *argv[]) {
     GtkBuilder      *builder; 
     GtkWidget       *windowMain;
+    /*--- CSS -----------------*/
+    GtkCssProvider  *provider;
+    GdkDisplay      *display;
+    GdkScreen       *screen;
+    /*-------------------------*/
  
     gtk_init(&argc, &argv);
  
@@ -13,21 +20,33 @@ int main(int argc, char *argv[]) {
  
     windowMain = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
     gtk_builder_connect_signals(builder, NULL);
+
+    /*---------------- CSS ------------------------------------------------*/
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    gsize bytes_written, bytes_read;
+
+    const gchar* home = "style/style.css";  
+    GError *error = 0;
+  
+    gtk_css_provider_load_from_path (provider,
+        g_filename_to_utf8(home, strlen(home), &bytes_read, &bytes_written, &error),NULL);
+    /*---------------- END CSS ------------------------------------------------*/
  
     g_object_unref(builder);
- 
     gtk_widget_show(windowMain);                
     gtk_main();
  
     return 0;
 }
  
-// called when window is closed
-void on_window_main_destroy() {
-    gtk_main_quit();
-}
-
-void on_btn_exit_clicked() {
+void on_exit_clicked() {
+    if(pid == 0){
+       kill(pid, SIGTERM);
+    }
     gtk_main_quit();
 }
 
--- src/pending.c
+++ src/pending.c
@@ -1,19 +1,41 @@
 #include <gtk/gtk.h>
+#include <string.h>
+
 int main() {
     GtkBuilder      *builder; 
-    GtkWidget       *secondWindow;
+    GtkWidget       *windowPending;
+    /*--- CSS -----------------*/
+    GtkCssProvider  *provider;
+    GdkDisplay      *display;
+    GdkScreen       *screen;
+    /*-------------------------*/
  
     gtk_init(NULL, NULL);
  
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/window_pending.glade", NULL);
  
-    secondWindow = GTK_WIDGET(gtk_builder_get_object(builder, "window_pending"));
+    windowPending = GTK_WIDGET(gtk_builder_get_object(builder, "window_pending"));
     gtk_builder_connect_signals(builder, NULL);
+
+    /*---------------- CSS ------------------------------------------------*/
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    gsize bytes_written, bytes_read;
+
+    const gchar* home = "style/style.css";  
+    GError *error = 0;
+  
+    gtk_css_provider_load_from_path (provider,
+        g_filename_to_utf8(home, strlen(home), &bytes_read, &bytes_written, &error),NULL);
+    /*---------------- END CSS ------------------------------------------------*/
  
     g_object_unref(builder);
  
-    gtk_widget_show(secondWindow);                
+    gtk_widget_show(windowPending);                
     gtk_main();
  
     return 0;
