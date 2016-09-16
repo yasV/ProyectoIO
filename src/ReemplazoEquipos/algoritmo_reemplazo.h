fea9110a8a015ea46485db93b835df30463af072
Amanda <sofia.29@hotmail.es>
2016-09-15 19:27:54 -0600

    Interfaz con varios planes

--- src/ReemplazoEquipos/algoritmo_reemplazo.h
+++ src/ReemplazoEquipos/algoritmo_reemplazo.h
@@ -11,10 +11,17 @@ char row[9];
 FILE * fileTableData;
 
 typedef struct  {
-  int proximo[31];
+  int year[31];
   int value;	
-  int min_year;
+  int position;
 } FinalTable;
+
+
+typedef struct 
+{
+	int value;
+	int year;
+}candidate;
 
 typedef struct  {
   int year;	
@@ -109,6 +116,48 @@ void startFill(int matrizD[][3],char *address) {
 	setMatriz(matrizD);
 }
 
+
+void mostrar_respuesta(FinalTable finalData[timeLimit+1]){
+	 int stop = 0;
+	 int contador = 0;
+	 FinalTable answer = finalData[contador];
+	 printf("%d",0);
+	 while(stop==0){
+	 	
+	 	if (answer.position==0){
+	 		stop = 1;
+	 	}
+	 	else{
+	 		printf("-%d",answer.year[answer.position-1]);
+	 		answer = finalData[answer.year[answer.position-1]];
+	 	}
+	 }
+	 				printf("______________________________________\n");
+
+}
+
+void planes(FinalTable finalData[timeLimit+1]){
+	
+		for (int i=0;i<=timeLimit;i++){
+			if (finalData[i].position > 1){
+				int x = finalData[i].position;
+			for (x;x>=1;x--){
+				printf("%d\n",x );
+				 mostrar_respuesta(finalData);
+				 finalData[i].position = finalData[i].position-1;
+			
+			if (finalData[i].position==0){
+				finalData[i].position=1;
+				x = 0;
+			
+			}	
+		}
+	}
+}
+}
+
+
+
 void replaceAlgorithm(InitialTable initialData[totalUsefulLife], FinalTable finalData[timeLimit+1]) {
 
 	int cost[totalUsefulLife];
@@ -124,30 +173,54 @@ void replaceAlgorithm(InitialTable initialData[totalUsefulLife], FinalTable fina
 		mantain = 0;
 	}
 
-	for (int i = timeLimit; i>=0 ;i--) {
-		int posible = 0;
+	for (int i = timeLimit;i>=0;i--){
+		int min = 0;
 		int flag = 0;
-		FinalTable object;
-		object.value = 0;
-		object.min_year = 0;
-
-		for (int x = i+1; x <= timeLimit; x++) {
-			FinalTable lastObject = finalData[x];
-			if (flag < totalUsefulLife) {
+		candidate options[totalUsefulLife];
+		candidate posibilites;
+		posibilites.value = 0;
+		posibilites.year = i;
+		FinalTable answer;
+		for (int x = i+1;x<=timeLimit;x++){
+			if (flag < totalUsefulLife){
+				posibilites.value = cost[(x-i)-1] + finalData[x].value;
+				posibilites.year = x;
+				options[flag] = posibilites;
 				flag ++;
-				if (object.value == 0){
-					object.value = cost[(x-i)-1] + lastObject.value;
-					object.min_year = x;
-				} 
-				else {
-					posible = cost[(x-i)-1] + lastObject.value;
-					if (posible<object.value){
-						object.value = posible;
-						object.min_year = x;
-					}
-				}
 			}
 		}
-		finalData[i] = object;
-	}
+
+
+		if (flag == 0){
+			answer.value = posibilites.value;
+			answer.year[0] = posibilites.year;
+			answer.position = 0;
+		}
+
+		for (int i = 0;i<flag;i++){
+			candidate aux;
+			aux = options[i];
+			if (i==0){
+				min = aux.value;
+				answer.year[0] = aux.year;
+				answer.position = 1;
+			}
+			else{
+				if (aux.value == min){
+					answer.year[answer.position] = aux.year;
+					answer.position ++;
+				}
+
+				if (aux.value<min){
+					answer.year[0] = aux.year;
+					answer.position = 1;
+					min = aux.value;
+				}
+
+			}
+			answer.value = min;
+		}
+		finalData[i] = answer;
+}
+
 }
